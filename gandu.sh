58e20f95595c67a18fddccc0ed149484308065a4
KABEER-AHMED <kabeerkd786@gmail.com>
2025-10-12 20:47:18 +0300

    created a normal executable shell file for testing

---
+++ gandu.sh
@@ -0,0 +1,19 @@
+whoami
+var="kabeer ahmed is the king"
+
+echo "who is the king ?"
+echo $var
+kabeer=0
+Allah=1
+
+echo can kabeer even think about being equal to allah? 
+if [ $kabeer -ge $Allah ]
+then
+echo loool ur code wrong bro 
+else
+echo see he can never be equal
+
+fi 
+
+
+
